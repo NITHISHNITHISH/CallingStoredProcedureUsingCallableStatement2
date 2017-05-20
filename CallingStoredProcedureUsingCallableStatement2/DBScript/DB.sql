77a529174091f68a2e4efa75cfeb3934a0464afd
Kishan Kumar <Kishan.Kumar@ingramcontent.com>
2017-05-20 13:24:54 +0530

    CallingStoredProcedureUsingCallableStatement2 source code

---
+++ CallingStoredProcedureUsingCallableStatement2/DBScript/DB.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `employee_table` (
+  `employee_id` int(11) NOT NULL auto_increment,
+  `employee_name` varchar(60) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `salary` double default NULL,
+  `date_of_joining` datetime default NULL,
+  `bonus` decimal(10,0) default NULL,
+  PRIMARY KEY  (`employee_id`)
+) ;
+
+
+DROP PROCEDURE IF EXISTS getEmployeeById;
+CREATE PROCEDURE `getEmployeeById`(IN empId INT)
+BEGIN
+	SELECT *FROM employee_table WHERE employee_id =empId;
+END;
